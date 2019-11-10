d5c5bb1b10b522da428dc7cfb0be9c5b26100a63
XinYe <amleaf@gmail.com>
2019-11-10 14:33:17 +0800

    amplify update

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to aba dashboard App"/>
+    <HelloWorld msg="Welcome to aba dashboard App in SG"/>
   </div>
 </template>
 
