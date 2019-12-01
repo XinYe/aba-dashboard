ea10d6dd569c49076ba17ff3b190567eac97ca9c
XinYe <amleaf@gmail.com>
2019-12-01 16:29:03 +0800

    integrate with amplify via aws-exports

--- src/main.js
+++ src/main.js
@@ -5,12 +5,19 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
+// amplify configuration
+import Amplify, * as AmplifyModules from 'aws-amplify'
+import { AmplifyPlugin } from 'aws-amplify-vue'
+import aws_exports from './aws-exports'
+Amplify.configure(aws_exports)
+
 // global styles (for auth ui)
 require('./assets/main.css')
 
 Vue.config.productionTip = false;
 
 Vue.use(ElementUI, { size: 'small', zIndex: 3000 });
+Vue.use(AmplifyPlugin, AmplifyModules)
 
 new Vue({
   router,
